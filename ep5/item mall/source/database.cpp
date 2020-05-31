3c5462bbdb7a264c2064a751e240d8ca5045c4a9
Bowie <61043863+426f776965@users.noreply.github.com>
2020-05-31 01:52:25 -0400

    went back to old code
    ini method is too unstable

--- ep5/item mall/source/database.cpp
+++ ep5/item mall/source/database.cpp
@@ -22,16 +22,9 @@ BOOL CDataBase::LinkDataBase()
 	{
 		OutputDebugString(L"");
 	}
-	LPCTSTR szPath = L".\\ncash.ini"; //define the path of Db.ini as the current directory and create a variable
-	WCHAR szUsername[32]; //initialize the username variable
-	WCHAR szPassword[32]; //initialize the password variable
-	GetPrivateProfileString(L"NCash", L"Uid", L"", szUsername, 32, szPath); //parse the ini and return the username
-	GetPrivateProfileString(L"NCash", L"Pwd", L"", szPassword, 32, szPath); //parse the ini and return the password
 	CString szLink;
-	szLink.Format(L"Provider=SQLOLEDB;Server=127.0.0.1;Database=PS_UserData;"); //define the provider, ip, and database
-	CString szUid = szUsername; //convert the username from wchar_t to a string
-	CString szPwd = szPassword; //convert the password from wchar_t to a string
-	m_pConnect->Open(_bstr_t(szLink), _bstr_t(szUid), _bstr_t(szPwd), NULL); //pass the string and the login variables
+	szLink.Format(L"Provider=SQLOLEDB;Server=127.0.0.1;Database=PS_UserData;Uid=yourusername1234;Pwd=yourpassword1234;");
+	m_pConnect->Open(_bstr_t(szLink), _bstr_t(L""), _bstr_t(L""), adModeUnknown);
 	return TRUE;
 }
 
