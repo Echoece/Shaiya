c729c773d78e4042d6a69c8dc8f1bd44871b68e9
Bowie <61043863+426f776965@users.noreply.github.com>
2020-06-11 22:28:03 -0400

    Update pch.h

--- ep6/npc to-go/source/pch.h
+++ ep6/npc to-go/source/pch.h
@@ -4,15 +4,12 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 //the main function
-void npcHook();
+void npcFunc();
 //definition for the hook function
 void* Hook(DWORD address, int length, DWORD target);
-int ptrValue1 = 1;
-int ptrValue2 = 257;
-DWORD npcReturn1 = 0x58712A;
 //store the npc window ids in an array of 6 integers
 int npcArray[6] = { 102, 121, 103, 110, 111, 101 };
 DWORD npcCall = 0x51C070;
-DWORD npcReturn2 = 0x522165;
+DWORD npcReturn = 0x522165;
 
 #endif
